f55d4e668be3e9716fe30c878490cadac9802232
htlin2 <htlin2@gmail.com>
2019-01-10 18:30:06 -0800

    implemented row & column check for winner

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ const board = [
 ];
 let currentPlayer;
 
+function hasWinner(board) {
+  for (let i = 0; i < 3; i++) {
+    const row = board[i];
+    const col = [];
+    for (let j = 0; j < 3; j++) {
+      col.push(board[j][i]);
+    }
+    if (row.every(e => e === 'O') || row.every(e => e === 'X')) return true;
+    if (col.every(e => e === 'O') || col.every(e => e === 'X')) return true;
+  }
+
+  if (dia.every(e => e === 'O') || dia.every(e => e === 'X')) return true;
+  return false;
+}
+
 function setName(playerNum, cb) {
   rl.question(`May I have your name, Player ${playerNum}? (Press enter to default to 'Player ${playerNum}') > `, (input) => {
     if (input !== '') {
@@ -23,28 +38,37 @@ function setName(playerNum, cb) {
   })
 }
 
-function isInputValid(input) {
-  if (input.length < 2) return false;
-  if (input[0] < 0 || input[0] > 2) return false;
-  if (input[1] < 0 || input[1] > 2) return false;
+function isInputValid(input, board) {
+  if (input.length < 2 || input.length > 2) return false;
+  const num1 = parseInt(input[0]);
+  const num2 = parseInt(input[1]);
+  if (!num1 || num1 < 0 || num1 > 2) return false;
+  if (!num2 || num2 < 0 || num2 > 2) return false;
+  if (board[num1][num2] !== '') return false;
   return true;
 }
 
 function startGame() {
   console.table(board);
   rl.question(`'${currentPlayer}' enter row & column index: (row, col) > `, (input) => {
-    if (!isInputValid(input)) {
+    if (!isInputValid(input, board)) {
       console.log(`${input} is not Valid`);
       return startGame();
     }
+
     const rowIndex = input[0];
     const colIndex = input[1];
+
     if (currentPlayer === playerName[0]) {
       board[rowIndex][colIndex] = 'O';
       currentPlayer = playerName[1];
     } else {
       board[rowIndex][colIndex] = 'X';
       currentPlayer = playerName[0];
+    }
+    if (hasWinner(board)) {
+      console.log('We have a winner');
+      rl.close();
     }
     startGame();
   })
